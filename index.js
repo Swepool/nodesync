cff4b6b990b8cd41a2b3b6349f942beecd1b618f
Swepool <36674091+Swepool@users.noreply.github.com>
2022-01-05 01:39:20 +0100

    fix

--- index.js
+++ index.js
@@ -2,53 +2,58 @@ import fetch from "node-fetch";
 import fs from 'fs'
 
 //Fetch list of nodes
-fetch("https://raw.githubusercontent.com/kryptokrona/kryptokrona-nodes-list/master/nodes.json")
-  .then(res => res.json())
-  .then (data => {
+setInterval(async function() {
 
-    //Fetch data from all nodes in list
-    for(const node of data.nodes) {
-      fetch("http://" + node.url + ":11898/getinfo")
-        .catch(error => {
-          throw(error);
-        })
-        .then(res => res.json())
-        .then(data => {
+  fetch("https://raw.githubusercontent.com/kryptokrona/kryptokrona-nodes-list/master/nodes.json")
+    .then(res => res.json())
+    .then (data => {
 
-          //only continue with nodes replying with status: "OK" and synced
-          //if(data.status === "OK" && data.synced)
-          renderNode(node, data)
-        })
-      process.on('uncaughtException', function(err) {
-        console.log('problem', err);
-      });
-    }
-  })
+      //Fetch data from all nodes in list
+      for(const node of data.nodes) {
+        fetch("http://" + node.url + ":11898/getinfo")
+          .catch(error => {
+            throw(error);
+          })
+          .then(res => res.json())
+          .then(data => {
 
-let nodes = []
+            //only continue with nodes replying with status: "OK" and synced
+            //if(data.status === "OK" && data.synced)
+            renderNode(node, data)
+          })
+        process.on('uncaughtException', function(err) {
+          console.log('problem', err);
+        });
+      }
+
+      let nodes = []
 //Render all the nodes to the table
-async function renderNode(node, data) {
-  nodes.push({
-    nodeName: node.name,
-    nodeHeight: data.height,
-    connectionsIn: data.incoming_connections_count,
-    connectionsOut: data.outgoing_connections_count,
-    nodeSynced: data.synced,
-    nodeStatus: data.status
-  })
-  let jsonString = JSON.stringify({ ...nodes })
-  console.log(jsonString)
-  test(jsonString)
-}
+      async function renderNode(node, data) {
+        nodes.push({
+          nodeName: node.name,
+          nodeHeight: data.height,
+          connectionsIn: data.incoming_connections_count,
+          connectionsOut: data.outgoing_connections_count,
+          nodeSynced: data.synced,
+          nodeStatus: data.status
+        })
+        let jsonString = JSON.stringify({ ...nodes })
+        console.log(jsonString)
+        test(jsonString)
+      }
 
+    })
+}, 15000)
 
-function test(array) {
+async function test(array) {
   fs.writeFile('db.json', array, (err) => {
     if (err) {
       throw err;
     }
     console.log("JSON data is saved.");
   });
+
 }
+
 
 
